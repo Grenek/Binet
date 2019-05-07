b644299ffe59ea10b4e146812f4acf199feed600
Grenek <grenek075@gmail.com>
2019-05-07 12:35:02 +0500

    Initial Commit

---
+++ Binet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Binet
+//
+//  Created by Вадим on 07/05/2019.
+//  Copyright © 2019 Shamratov Vadim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
